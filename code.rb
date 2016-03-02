dcbfec26a61281fa3a7bdf600a684030dd3ba797
Evan Watterud <cod.videos@hotmail.com>
2016-03-01 21:01:45 -0500

    Finished using inheritance

--- code.rb
+++ code.rb
@@ -47,19 +47,125 @@ class SuperHero
 end
 
 class Speedster < SuperHero
+  def backstory
+    "He wasn't this fast before"
+  end
+
+  def powers
+    "Going fast"
+  end
+
+  def weakness
+    "stuns"
+  end
+
+  def speed_in_mph
+    super*1000
+  end
 end
 
 class Brawler < SuperHero
+  def backstory
+    "He beat up some people"
+  end
+
+  def powers
+    "He's pretty strong"
+  end
+
+  def weakness
+    "mice"
+  end
+
+  def health
+    super * 20
+  end
 end
 
 class Detective < SuperHero
+  def backstory
+    "He did some stuff I don't know about"
+  end
+
+  def powers
+    "detecting things"
+  end
+
+  def weakness
+    "Luke Bayne"
+  end
+
+  def speed_in_mph
+    super/6
+  end
 end
 
 class Demigod < SuperHero
+  def backstory
+    "Some accident made him crazy strong"
+  end
+
+  def powers
+    "Can do pretty much anything"
+  end
+
+  def weakness
+    "Doesn't understand humans"
+  end
+
+  def home
+    "Cosmic Plane"
+  end
 end
 
 class JackOfAllTrades < SuperHero
+  def backstory
+    "Can't think of anything"
+  end
+
+  def powers
+    "Good at everything, but not amazing at anything"
+  end
+
+  def weakness
+    "nothing"
+  end
+
+  def species
+    "Alien"
+  end
+
+  def home
+    "Venus"
+  end
+
+  def psychic?
+    true
+  end
 end
 
 class WaterBased < SuperHero
+  def backstory
+    "Likes water"
+  end
+
+  def powers
+    "Water stuff"
+  end
+
+  def weakness
+    "being out of water, duh"
+  end
+
+  def home
+    super + "'s Oceans"
+  end
+
+  def fans_per_thousand
+    super/100
+  end
+
+  def psychic?
+    true
+  end
 end
